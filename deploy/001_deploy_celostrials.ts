49508a01abcd3cc6e136445fe1c9ec4c1155aaa0
bridgerz <bridgerzoske@gmail.com>
2022-08-04 17:27:02 -0700

    Deploy testnet

--- deploy/001_deploy_celostrials.ts
+++ deploy/001_deploy_celostrials.ts
@@ -4,6 +4,9 @@ import { deployments, ethers } from "hardhat"
 import { Celostrials__factory } from "../types"
 
 const func: DeployFunction = async function (hardhat: HardhatRuntimeEnvironment) {
+  let celostrialsAddress = (await hardhat.deployments.getOrNull("Celostrials"))?.address
+  if (celostrialsAddress) return
+
   await deployments.deploy("Celostrials", {
     from: (await hardhat.ethers.getSigners())[0].address,
     args: [],
